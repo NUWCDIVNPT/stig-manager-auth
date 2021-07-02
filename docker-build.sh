48256f6009d73ebbebb9556dd8b97349f6f0690a
csmig <csmig@csmig.com>
2021-07-02 12:54:28 -0400

    Keycloak 14, rbac auth flow and scopes

---
+++ docker-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build --tag stig-manager-auth:${1:-dev} .
---
+++ export-realm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# The command below will start a second Keycloak instace on a port offset from the other instance.
+# The second instance will export the indicated realm and attach itself to the terminal.
+# Keycloak will log the message: "Admin console listening on http://127.0.0.1:<port>"
+# You should press Ctrl-C after this message is logged.
+
+docker exec -it ${1:-stig-manager-auth} /opt/jboss/keycloak/bin/standalone.sh \
+-Djboss.socket.binding.port-offset=100 \
+-Dkeycloak.migration.action=export \
+-Dkeycloak.migration.provider=singleFile \
+-Dkeycloak.migration.realmName=stigman \
+-Dkeycloak.migration.usersExportStrategy=REALM_FILE \
+-Dkeycloak.migration.file=/tmp/stigman_realm.json
+
+# Press Ctrl-C here to stop Keycloak and execute this line
+docker cp ${1:-stig-manager-auth}:/tmp/stigman_realm.json .
+
