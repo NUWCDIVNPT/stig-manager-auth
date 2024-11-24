3254d6c899d2b52058e368b56cfd67caff07207d
csmig <carlsmigielski@gmail.com>
2024-11-24 14:04:03 -0500

    update to Keycloak 26.0.6; realm cleanup

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker container rm stig-manager-auth --force
-docker run --name stig-manager-auth -p ${2:-8080}:8080 -p 8443:8443 -d stig-manager-auth:${1:-dev} start-dev --import-realm
+docker run -d --name stig-manager-auth -p ${2:-8080}:8080 stig-manager-auth:${1:-dev}
 docker logs -f stig-manager-auth
